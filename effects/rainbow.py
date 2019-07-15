b08ab22b43dcdc51a3ed4462361a4524fc0756cc
John Chase <ratteal@gmail.com>
2019-07-15 21:44:18 +0100

    Tweaks to the rainbow.

--- effects/rainbow.py
+++ effects/rainbow.py
@@ -7,25 +7,24 @@ from colorsys import hsv_to_rgb
 
 class Rainbow(AbstractEffect):
     """
-    A slowly changing rainbow.
+    A slowly moving rainbow.
     """
 
-    SPACING = 360.0 / 16.0
-
     def __init__(self, canvas, debug):
-        self.__hue = 0
+        self.__SPACING = 360.0 / canvas.get_size()
+        self.__base_hue = 0
         super(Rainbow, self).__init__("rainbow", 0.01, canvas, debug)
 
-    def show_rainbow(self, hue):
+    def show_rainbow(self, base_hue):
         for x in range(self.canvas.get_size()):
-            offset = x * self.SPACING
-            h = ((hue + offset) % 360) / 360.0
-            pixel = [int(c * 255) for c in hsv_to_rgb(h, 1.0, 1.0)]
+            offset = x * self.__SPACING
+            hue = ((base_hue + offset) % 360) / 360.0
+            pixel = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
             self.canvas.set_pixel(x, Pixel.from_tuple(pixel))
 
     def compose(self):
-        self.__hue = int(time() * 100) % 360
-        self.show_rainbow(self.__hue)
+        self.__base_hue = int(time() * 100) % 360
+        self.show_rainbow(self.__base_hue)
 
     def print_debug(self):
-        print("Hue: {0}".format(self.__hue))
+        print("Base Hue: {0}".format(self.__base_hue))
