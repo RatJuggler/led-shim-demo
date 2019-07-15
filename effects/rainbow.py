c44a80d011324e94598651785866d04e3094c67a
John Chase <ratteal@gmail.com>
2019-07-15 22:34:26 +0100

    Clarify hue spacing.

--- effects/rainbow.py
+++ effects/rainbow.py
@@ -11,19 +11,19 @@ class Rainbow(AbstractEffect):
     """
 
     def __init__(self, canvas, debug):
-        self.__SPACING = 360.0 / canvas.get_size()
+        self.__HUE_SPACING = 360.0 / canvas.get_size()
         self.__base_hue = 0
         super(Rainbow, self).__init__("rainbow", 0.01, canvas, debug)
 
     def show_rainbow(self, base_hue):
-        for x in range(self.canvas.get_size()):
-            offset = x * self.__SPACING
-            hue = ((base_hue + offset) % 360) / 360.0
-            pixel = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
-            self.canvas.set_pixel(x, Pixel.from_tuple(pixel))
+        for i in range(self.canvas.get_size()):
+            offset = self.__HUE_SPACING * i
+            hue = (base_hue + offset) % 360
+            pixel = [int(c * 255) for c in hsv_to_rgb(hue / 360.0, 1.0, 1.0)]
+            self.canvas.set_pixel(i, Pixel.from_tuple(pixel))
 
     def compose(self):
-        self.__base_hue = int(time() * 100) % 360
+        self.__base_hue = int(time() * 100) % 360  # Sync with time to move the rainbow.
         self.show_rainbow(self.__base_hue)
 
     def print_debug(self):
