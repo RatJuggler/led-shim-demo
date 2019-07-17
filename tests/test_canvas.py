a57e52ca92d1febc49cdd22849b5ba5aac1ef730
John Chase <ratteal@gmail.com>
2019-07-17 21:59:19 +0100

    Add some tests for get_pixel.

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,9 +1,10 @@
 import unittest
 
 from ledshimeffects.canvas import Canvas
+from ledshimeffects.colours import Colours
 
 
-class TestCanvas(unittest.TestCase):
+class TestCanvasConstructor(unittest.TestCase):
 
     def test_constructor_valid_size(self):
         canvas = Canvas(10)
@@ -16,6 +17,25 @@ class TestCanvas(unittest.TestCase):
     def test_constructor_enormous_size(self):
         size = 1000
         self.assertRaises(ValueError, Canvas, size)
+
+
+class TestCanvasGetPixel(unittest.TestCase):
+
+    def setUp(self):
+        self.canvas = Canvas(2)
+        self.canvas.set_pixel(1, Colours.BLUE)
+
+    def test_get_pixel_none(self):
+        pixel = self.canvas.get_pixel(0)
+        self.assertIsNone(pixel)
+
+    def test_get_pixel_color(self):
+        pixel = self.canvas.get_pixel(1)
+        self.assertEqual(pixel, Colours.BLUE)
+
+    def test_get_pixel_invalid(self):
+        with self.assertRaises(ValueError):
+            self.canvas.get_pixel(99)
 
 
 if __name__ == '__main__':
