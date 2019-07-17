d1f18a651549b66a34bdf5f19a684b9086b64377
John Chase <ratteal@gmail.com>
2019-07-17 20:52:31 +0100

    Test some other default colours.

--- tests/test_colours.py
+++ tests/test_colours.py
@@ -1,6 +1,7 @@
-from colours import Colours
-
 import unittest
+
+from ledshimeffects.colours import Colours
+from ledshimeffects.pixel import Pixel
 
 
 class TestColours(unittest.TestCase):
@@ -9,6 +10,19 @@ class TestColours(unittest.TestCase):
         self.assertEqual(Colours.RED.get_r(), 255)
         self.assertEqual(Colours.RED.get_g(), 0)
         self.assertEqual(Colours.RED.get_b(), 0)
+        self.assertEqual(Colours.RED.get_brightness(), Pixel.DEFAULT_BRIGHTNESS)
+
+    def test_green(self):
+        self.assertEqual(Colours.GREEN.get_r(), 0)
+        self.assertEqual(Colours.GREEN.get_g(), 255)
+        self.assertEqual(Colours.GREEN.get_b(), 0)
+        self.assertEqual(Colours.GREEN.get_brightness(), Pixel.DEFAULT_BRIGHTNESS)
+
+    def test_blue(self):
+        self.assertEqual(Colours.BLUE.get_r(), 0)
+        self.assertEqual(Colours.BLUE.get_g(), 0)
+        self.assertEqual(Colours.BLUE.get_b(), 255)
+        self.assertEqual(Colours.BLUE.get_brightness(), Pixel.DEFAULT_BRIGHTNESS)
 
     def test_colours(self):
         self.assertEqual(len(Colours.COLOURS), 12)
