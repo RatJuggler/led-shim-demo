cff922acd51f2cb163311e5bae6ea2fde7c8009f
John Chase <ratteal@gmail.com>
2019-07-20 19:03:16 +0100

    First attempt at adding command line options using Click.

--- ledshimeffects/__main__.py
+++ ledshimeffects/__main__.py
@@ -2,6 +2,7 @@
 
 # Application to display a series of effects on a Pimoroni led-shim.
 
+import click
 from random import randint
 from time import sleep
 
@@ -11,10 +12,6 @@ from canvas import Canvas
 from effects import Candle, BinaryClock, CheerLights, GradientGraph, Rainbow, RandomBlink, SolidColours
 
 NUM_PIXELS = 28     # Number of LEDs on the shim.
-INVERT = True       # Invert the display depending on which way round the shim is to be viewed.
-
-EFFECT_TIME = 10    # Time to show each effect, in seconds.
-DEBUG = False       # Show additional output on composing.
 
 #ledshim.set_clear_on_exit()
 
@@ -40,14 +37,26 @@ def cycle_effects():
     return effects[effect_no]
 
 
-def display_effects():
+@click.command()
+@click.option('-c', '--cycle', 'display', flag_value="CYCLE", default=True, help="Display the effects in a cycle (the default")
+@click.option('-r', '--random', 'display', flag_value="RANDOM", help="Display the effects randomly")
+@click.option('-t', '--effect_time', type=int, default=10, help="How long to display each effect for, in seconds", show_default=True)
+@click.option('-i', '--invert', is_flag=True, help="Change the display orientation")
+@click.option('-d', '--debug', is_flag=True, help="Show additional debug information")
+def display_effects(display, effect_time, invert, debug):
+    """
+    Show various effects on a Pimoroni LED shim.
+    """
     try:
         show_time = 0
         effect = effects[0]
         while True:
             if show_time <= 0:
-                effect = cycle_effects()
-                show_time = EFFECT_TIME / effect.get_speed()
+                if display == "CYCLE":
+                    effect = cycle_effects()
+                if display == "RANDOM":
+                    effect = random_effect()
+                show_time = effect_time / effect.get_speed()
                 print(str(effect))
             effect.compose()
             if effect.is_debug():
@@ -55,7 +64,7 @@ def display_effects():
                 print(repr(canvas))
             for i in range(canvas.get_size()):
                 pixel = canvas.get_pixel(i)
-                position = (canvas.get_size() - 1 - i) if INVERT else i
+                position = (canvas.get_size() - 1 - i) if invert else i
 #                ledshim.set_pixel(position, pixel.get_r(), pixel.get_g(), pixel.get_b(), pixel.get_brightness())
 #            ledshim.show()
             show_time -= 1
