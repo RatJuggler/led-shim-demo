b1698ba3a3ec849530d39cb2a590a4ffedca2c25
John Chase <ratteal@gmail.com>
2019-07-22 16:32:33 +0100

    Pull the long description from the README and add other meta-data.

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+from codecs import open
+from os.path import abspath, dirname, join
+
 from setuptools import setup
 
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
+
 setup(
-    name = 'ledshimeffects',
-    version = '0.0.1',
+    name='ledshimeffects',
+    version='0.0.1',
+    description='Show various effects on a Pimoroni LED shim.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='John Chase',
+    author_email='ratteal@gmail.com',
+    python_requires='>=3.5.3',
+    url='https://github.com/RatJuggler/led-shim-effects',
     packages = ['ledshimeffects'],
     entry_points = {
         'console_scripts': [
