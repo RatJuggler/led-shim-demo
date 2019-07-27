7729dd1b3ad3d9adfabed00c0e1664a3f1fdc8be
John Chase <ratteal@gmail.com>
2019-07-27 14:41:46 +0100

    Display options so we can see what was selected.

--- ledshimdemo/__main__.py
+++ ledshimdemo/__main__.py
@@ -12,14 +12,30 @@ NUM_PIXELS = 28     # The number of LEDs on the shim.
 ledshim.set_clear_on_exit()
 
 
+def show_options(show_effects: str, effect_time: int, brightness: int, invert: bool, log: str):
+    options = ["Active Options(",
+               "show_effects={0}, ".format(show_effects),
+               "effect_time={0}, ".format(effect_time),
+               "brightness={0}, ".format(brightness),
+               "invert={0}, ".format(invert),
+               "log={0}".format(log),
+               ")"]
+    return "".join(options)
+
+
 @click.command(help="Show various effects on a Pimoroni LED shim.")
 @click.version_option()
-@click.option('-s', '--show_effects', type=click.Choice(["CYCLE", "RANDOM"]), default="CYCLE", help="How the effects are displayed.", show_default=True)
-@click.option('-t', '--effect_time', type=click.IntRange(1, 3600), default=10, help="How long to display each effect for, in seconds.", show_default=True)
-@click.option('-b', '--brightness', type=click.IntRange(1, 10), default=8, help="How bright the effects will be (1-10).", show_default=True)
-@click.option('-i', '--invert', is_flag=True, help="Change the display orientation.")
-@click.option('-l', '--log', type=click.Choice(["NONE", "INFO", "EFFECT", "DEBUG"]), default="NONE", help="Show additional logging information.")
-def display_effects(show_effects, effect_time, brightness, invert, log):
+@click.option('-s', '--show_effects', type=click.Choice(["CYCLE", "RANDOM"]), default="CYCLE",
+              help="How the effects are displayed.", show_default=True)
+@click.option('-t', '--effect_time', type=click.IntRange(1, 3600), default=10,
+              help="How long to display each effect for, in seconds (1-3600).", show_default=True)
+@click.option('-b', '--brightness', type=click.IntRange(1, 10), default=8,
+              help="How bright the effects will be (1-10).", show_default=True)
+@click.option('-i', '--invert', is_flag=True,
+              help="Change the display orientation.")
+@click.option('-l', '--log', type=click.Choice(["NONE", "INFO", "EFFECT", "DEBUG"]), default="NONE",
+              help="Show additional logging information.")
+def display_effects(show_effects: str, effect_time: int, brightness: int, invert: bool, log: str):
     """
     Show various effects on a Pimoroni LED shim.
     :param show_effects: In a CYCLE or at RANDOM
@@ -29,6 +45,7 @@ def display_effects(show_effects, effect_time, brightness, invert, log):
     :param log: Set a logging level; NONE, INFO, EFFECT or DEBUG
     :return: No meaningful return
     """
+    print(show_options(show_effects, effect_time, brightness, invert, log))
     canvas = Canvas(NUM_PIXELS)
     effects = [Candle(canvas),
                GradientGraph(canvas),
