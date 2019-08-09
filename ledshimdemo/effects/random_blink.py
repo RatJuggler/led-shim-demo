7c3f71d4fd27f0388e1249ab4b2643d6d56129b1
John Chase <ratteal@gmail.com>
2019-08-09 19:25:17 +0100

    Ensure number of blinks is within number of LEDs.

--- ledshimdemo/effects/random_blink.py
+++ ledshimdemo/effects/random_blink.py
@@ -10,11 +10,13 @@ class RandomBlink(AbstractEffect):
     """
 
     def __init__(self, canvas):
+        # Number to blink must be within range of canvas size.
+        self.__number_to_blink = (canvas.get_size() // 5) + 1
         self.__pixels = None
         super(RandomBlink, self).__init__("random_blink", 0.05, canvas)
 
     def compose(self):
-        self.__pixels = sample(range(self.canvas.get_size()), randint(1, 5))
+        self.__pixels = sample(range(self.canvas.get_size()), randint(1, self.__number_to_blink))
         for i in range(self.canvas.get_size()):
             if i in self.__pixels:
                 self.canvas.set_pixel(i, Colours.OLDLACE)
