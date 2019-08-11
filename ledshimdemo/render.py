a4e4ab122c32c514591613f43cea609908f3c8e5
John Chase <ratteal@gmail.com>
2019-08-11 14:36:57 +0100

    Some more refactoring and tests on render code.

--- ledshimdemo/render.py
+++ ledshimdemo/render.py
@@ -7,19 +7,34 @@ import ledshim
 from abstract_effect import AbstractEffect
 
 
-def get_next_effect_no(effect_display: str, num_effects: int, effect_no: int):
+def get_next_effect(effect_display: str, effects: list):
+    """
+    Pick the next effect to display.
+    :param effect_display: In a CYCLE or at RANDOM
+    :param effects: A list of the effects to show
+    :return: The next effect to show
+    """
+    if "counter" not in get_next_effect.__dict__:
+        get_next_effect.effect_no = -1
     if effect_display == "CYCLE":
-        return (effect_no + 1) % num_effects
+        get_next_effect.effect_no = (get_next_effect.effect_no + 1) % len(effects)
     if effect_display == "RANDOM":
-        return randint(0, num_effects)
-    raise ValueError("Unknown effect_display type!")
+        get_next_effect.effect_no = randint(0, len(effects) -1)
+    return effects[get_next_effect.effect_no]
 
 
 def copy_to_shim(effect: AbstractEffect, invert: bool):
+    """
+    Display the effect canvas on the shim.
+    :param effect: being shown
+    :param invert: Orientation of the display
+    :return: No meaningful return
+    """
     for i in range(effect.canvas.get_size()):
         pixel = effect.canvas.get_pixel(i)
         position = (effect.canvas.get_size() - 1 - i) if invert else i
         ledshim.set_pixel(position, pixel.get_r(), pixel.get_g(), pixel.get_b(), pixel.get_brightness())
+    ledshim.show()
 
 
 def render(effect_display: str, effect_duration: int, effect_run: int, invert: bool, effects: list):
@@ -34,23 +49,24 @@ def render(effect_display: str, effect_duration: int, effect_run: int, invert: b
     """
     ledshim.set_clear_on_exit()
     show_time = 0
-    effect_no = len(effects) - 1
-    effect: AbstractEffect = effects[effect_no]
+    effect: AbstractEffect = effects[0]
     try:
         while effect_run >= 0:
             if show_time <= 0:
                 effect_run -= 1
-                effect_no = get_next_effect_no(effect_display, len(effects), effect_no)
-                effect = effects[effect_no]
+                effect = get_next_effect(effect_display, effects)
                 show_time = effect_duration / effect.get_speed()
                 logging.info(str(effect))
             effect.compose()
             logging.info(repr(effect))
             logging.debug(repr(effect.canvas))
             copy_to_shim(effect, invert)
-            ledshim.show()
             show_time -= 1
             sleep(effect.get_speed())
     except KeyboardInterrupt:
+        logging.info("Execution interrupted!")
+    except:
+        logging.exception("Unexpected exception!")
+    finally:
         ledshim.clear()
         ledshim.show()
