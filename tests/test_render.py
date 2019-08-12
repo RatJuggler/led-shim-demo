f7406628576ab1a64aa4df42531d2cb79e616f49
John Chase <ratteal@gmail.com>
2019-08-12 14:47:13 +0100

    Ensure test can find render class when run from the command line.

--- tests/test_render.py
+++ tests/test_render.py
@@ -16,7 +16,7 @@ class TestRender(unittest.TestCase):
 
     def test_get_next_effect_cycle(self):
         sys.modules['ledshim'] = mock.Mock()
-        from render import get_next_effect
+        from ledshimdemo.render import get_next_effect
         effect = get_next_effect('CYCLE', self.effects)
         self.assertIsInstance(effect, SolidColours)
         effect = get_next_effect('CYCLE', self.effects)
@@ -28,17 +28,17 @@ class TestRender(unittest.TestCase):
 
     def test_get_next_effect_random(self):
         sys.modules['ledshim'] = mock.Mock()
-        from render import get_next_effect
+        from ledshimdemo.render import get_next_effect
         effect = get_next_effect('RANDOM', self.effects)
         self.assertTrue(isinstance(effect, (SolidColours, RandomBlink, Rainbow)))
 
     def test_copy_to_shim(self):
         sys.modules['ledshim'] = mock.Mock()
-        from render import copy_to_shim
+        from ledshimdemo.render import copy_to_shim
         copy_to_shim(self.effects[0], False)
 
     def test_render(self):
         sys.modules['ledshim'] = mock.Mock()
-        from render import render
+        from ledshimdemo.render import render
         render("CYCLE", 3, 1, False, self.effects)
         self.assertTrue(True)
