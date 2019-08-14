f65fa9ebba34add60f4b60b4d0da25166f4c1d26
John Chase <ratteal@gmail.com>
2019-08-14 21:50:01 +0100

    Corrections to reduce ignored issues.

--- ledshimdemo/effects/__init__.py
+++ ledshimdemo/effects/__init__.py
@@ -7,3 +7,13 @@ from .gradient_graph import GradientGraph
 from .rainbow import Rainbow
 from .random_blink import RandomBlink
 from .solid_colours import SolidColours
+
+__all__ = ['AbstractEffect',
+           'BinaryClock',
+           'Candle',
+           'CheerLights',
+           'ColouredLights',
+           'GradientGraph',
+           'Rainbow',
+           'RandomBlink',
+           'SolidColours']
