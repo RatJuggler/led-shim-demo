50986a2e6cf6c42fbb7fb492044f137344c62d9f
John Chase <ratteal@gmail.com>
2019-08-16 17:07:43 +0100

    Fix the test.

--- tests/test_effects/test_digital_rain.py
+++ tests/test_effects/test_digital_rain.py
@@ -6,10 +6,10 @@ from ledshimdemo.effects.digital_rain import DigitalRain
 
 class TestDigitalRain(unittest.TestCase):
 
-    TEST_CANVAS_SIZE = 3  # type: int
+    TEST_CANVAS_SIZE = 14  # type: int
 
     def test_effect(self):
         canvas = Canvas(self.TEST_CANVAS_SIZE)
         effect = DigitalRain(canvas)
         effect.compose()
-        self.assertEqual(repr(effect), "DigitalRain(Tick:1)")
+        self.assertRegex(repr(effect), "^DigitalRain\\(Tick:1, Drops:\\[Drop\\(Falling:12, Trail:\\d, IntensityStep:\\d\\d, Speed:\\d\\)\\]\\)$")
