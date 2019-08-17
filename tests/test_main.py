d345c276aaa607c6faaf3085849aa52ee8a54c44
John Chase <ratteal@gmail.com>
2019-08-17 20:35:25 +0100

    Correct use of mock.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,11 +5,10 @@ import logging
 from click.testing import CliRunner
 
 sys.modules['smbus'] = mock.Mock()  # Mock the hardware layer to avoid errors.
+import ledshimdemo.__main__ as main
 
-from ledshimdemo.__main__ import display_effects
 
-
-@mock.patch('ledshimdemo.render.render')
+@mock.patch('ledshimdemo.__main__.render')
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -18,7 +17,7 @@ class Test(unittest.TestCase):
 
     def test_help(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--help'])
+        result = runner.invoke(main.display_effects, ['--help'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" --version ", result.output)
         self.assertIn(" --effect-list ", result.output)
@@ -34,53 +33,53 @@ class Test(unittest.TestCase):
 
     def test_version(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--version'])
+        result = runner.invoke(main.display_effects, ['--version'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn("display-effects, version ", result.output)
         render_mock.assert_not_called()
 
     def test_effect_list(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--effect-list'])
+        result = runner.invoke(main.display_effects, ['--effect-list'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn("Available Effects:\nBinaryClock\nCandle\nCheerLights\nColouredLights\nDigitalRain\nGradientGraph\nRainbow\nRandomBlink\nSolidColours\n", result.output)
         render_mock.assert_not_called()
 
     def test_default_options_no_log(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--test'])
+        result = runner.invoke(main.display_effects, ['--test'])
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output, "")
-        render_mock.assert_not_called()
+        render_mock.assert_called_once()
 
     def test_default_options_info_log(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--log-level', 'INFO',
-                                                 '--test'])
+        result = runner.invoke(main.display_effects, ['--log-level', 'INFO',
+                                                      '--test'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" - INFO - Logging level enabled!", result.output)
         self.assertIn(" - INFO - Active Options(effect-display=CYCLE, effect-duration=10, effect-run=24, brightness=8, invert=False, log-level=INFO)", result.output)
-        render_mock.assert_not_called()
+        render_mock.assert_called_once()
 
     def test_default_options_debug_log(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--log-level', 'DEBUG',
-                                                 '--test'])
+        result = runner.invoke(main.display_effects, ['--log-level', 'DEBUG',
+                                                      '--test'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" - DEBUG - Logging level enabled!", result.output)
         self.assertIn(" - INFO - Active Options(effect-display=CYCLE, effect-duration=10, effect-run=24, brightness=8, invert=False, log-level=DEBUG)", result.output)
-        render_mock.assert_not_called()
+        render_mock.assert_called_once()
 
     def test_all_options_verbose_log(self, render_mock):
         runner = CliRunner()
-        result = runner.invoke(display_effects, ['--effect-display', 'RANDOM',
-                                                 '--effect-duration', '180',
-                                                 '--effect-run', '240',
-                                                 '--brightness', '3',
-                                                 '--invert',
-                                                 '--log-level', 'VERBOSE',
-                                                 '--test'])
+        result = runner.invoke(main.display_effects, ['--effect-display', 'RANDOM',
+                                                      '--effect-duration', '180',
+                                                      '--effect-run', '240',
+                                                      '--brightness', '3',
+                                                      '--invert',
+                                                      '--log-level', 'VERBOSE',
+                                                      '--test'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" - VERBOSE - Logging level enabled!", result.output)
         self.assertIn(" - INFO - Active Options(effect-display=RANDOM, effect-duration=180, effect-run=240, brightness=3, invert=True, log-level=VERBOSE)", result.output)
-        render_mock.assert_not_called()
+        render_mock.assert_called_once()
