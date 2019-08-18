60cefe9cecdbc3a5980692457b919da9647abd3c
John Chase <ratteal@gmail.com>
2019-08-18 23:04:35 +0100

    A check in case no module classes found.

--- ledshimdemo/load_effect.py
+++ ledshimdemo/load_effect.py
@@ -1,18 +1,19 @@
 from importlib import import_module
 import os
 import pkgutil
+from typing import List
 
 from .abstract_effect import AbstractEffect
 
 
-def load_effects(*args, **kwargs):
+def load_effects(*args, **kwargs) -> List[AbstractEffect]:
     effects = []
     for (_, name, _) in pkgutil.iter_modules([os.path.dirname(__file__) + "/effects"]):
         effects.append(load_effect(name, *args, **kwargs))
     return effects
 
 
-def load_effect(effect_name, *args, **kwargs):
+def load_effect(effect_name: str, *args, **kwargs) -> AbstractEffect:
     try:
         if '.' in effect_name:
             module_name, class_name = effect_name.rsplit('.', 1)
@@ -23,12 +24,15 @@ def load_effect(effect_name, *args, **kwargs):
         effect_module = import_module('ledshimdemo.effects.' + module_name)
         if class_name is None:
             module_classes = list(filter(lambda x: x != 'AbstractEffect' and x.endswith('Effect'), dir(effect_module)))
-            class_name = module_classes[0]
+            if module_classes:
+                class_name = module_classes[0]
+            else:
+                class_name = effect_name.capitalize()
         effect_class = getattr(effect_module, class_name)
         instance = effect_class(*args, **kwargs)
     except (AttributeError, ImportError):
         raise ImportError('{} is not part of the effect collection!'.format(effect_name))
     else:
         if not issubclass(effect_class, AbstractEffect):
-            raise ImportError("We currently don't have {}, but you are welcome to send in the request for it!".format(effect_class))
+            raise ImportError("{} is not a valid effect class!".format(effect_class))
     return instance
