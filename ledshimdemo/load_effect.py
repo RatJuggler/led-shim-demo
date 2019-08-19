bb4415efd53b7f7ec1fd47bca224b1be28678d40
John Chase <ratteal@gmail.com>
2019-08-19 22:58:03 +0100

    Document the effect loading functions.

--- ledshimdemo/load_effect.py
+++ ledshimdemo/load_effect.py
@@ -6,6 +6,14 @@ from .abstract_effect import AbstractEffect
 
 
 def load_effects(effects_path, effects_package, *args, **kwargs) -> List[AbstractEffect]:
+    """
+    Load all the effects from a given path/package.
+    :param effects_path: path on the file system to the effects to load
+    :param effects_package: the name of the associated package
+    :param args: parameters for new effect instances
+    :param kwargs: parameters for new effect instances
+    :return: A list of instances of the effects loaded
+    """
     effects = []
     for (_, effect_module, _) in pkgutil.iter_modules([effects_path]):
         effects.append(load_effect(effects_package + effect_module, None, *args, **kwargs))
@@ -13,6 +21,15 @@ def load_effects(effects_path, effects_package, *args, **kwargs) -> List[Abstrac
 
 
 def load_effect(effect_module, effect_class: str, *args, **kwargs) -> AbstractEffect:
+    """
+    Load an effect class and create an instance of it.
+    An effect class must be a subclass of AbstractEffect and the name must end in "Effect".
+    :param effect_module: the module containing the effect class to load
+    :param effect_class: the name of the effect class to load
+    :param args: parameters for new effect instance
+    :param kwargs: parameters for new effect instance
+    :return: An instance of the effect class loaded
+    """
     try:
         effect_module = import_module(effect_module)
         if effect_class is None:
