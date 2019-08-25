38bf8c90d3671486ef1bb0c5deed16a58aa78c81
John Chase <ratteal@gmail.com>
2019-08-25 17:00:42 +0100

    Mock result should be tested for all calls.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from click.testing import CliRunner
 import ledshimdemo.__main__ as main
 
 
-@mock.patch('ledshimdemo.__main__.AbstractEffectDisplay.select_effect_display')
+@mock.patch('ledshimdemo.__main__.AbstractEffectDisplay')
 class TestMain(TestCase):
 
     def setUp(self):
@@ -13,7 +13,7 @@ class TestMain(TestCase):
             logging.root.removeHandler(handler)
         self.runner = CliRunner()
 
-    def test_help(self, select_effect_display_mock):
+    def test_help(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--help'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" --version ", result.output)
@@ -25,15 +25,17 @@ class TestMain(TestCase):
         self.assertIn(" --invert ", result.output)
         self.assertIn(" --log-level ", result.output)
         self.assertIn(" --help ", result.output)
-        select_effect_display_mock.assert_not_called()
+        effect_display_mock.select_effect_display.assert_not_called()
+        effect_display_mock.select_effect_display.return_value.render.assert_not_called()
 
-    def test_version(self, select_effect_display_mock):
+    def test_version(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--version'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn("display-effects, version ", result.output)
-        select_effect_display_mock.assert_not_called()
+        effect_display_mock.select_effect_display.assert_not_called()
+        effect_display_mock.select_effect_display.return_value.render.assert_not_called()
 
-    def test_effect_list(self, select_effect_display_mock):
+    def test_effect_list(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--effect-list'])
         self.assertEqual(result.exit_code, 0)
         effects = ["Available Effects:",
@@ -48,31 +50,35 @@ class TestMain(TestCase):
                    "SolidColours   - A sequence of solid colours."]
         effects_list = "\n".join(effects)
         self.assertIn(effects_list, result.output)
-        select_effect_display_mock.assert_not_called()
+        effect_display_mock.select_effect_display.assert_not_called()
+        effect_display_mock.select_effect_display.return_value.render.assert_not_called()
 
-    def test_default_options_default_log(self, select_effect_display_mock):
+    def test_default_options_default_log(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, [])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" - INFO - Logging level enabled!", result.output)
         self.assertIn(" - INFO - Active Options(effect-display=CYCLE, effect-duration=10 secs, repeat-run=1, "
                       "brightness=8, invert=False, log-level=INFO, lead=False, effects_selected=ALL)", result.output)
-        select_effect_display_mock.assert_called_once()
+        effect_display_mock.select_effect_display.assert_called_once()
+        effect_display_mock.select_effect_display.return_value.render.assert_called_once()
 
-    def test_default_options_warning_log(self, select_effect_display_mock):
+    def test_default_options_warning_log(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--log-level', 'WARNING'])
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output, "")
-        select_effect_display_mock.assert_called_once()
+        effect_display_mock.select_effect_display.assert_called_once()
+        effect_display_mock.select_effect_display.return_value.render.assert_called_once()
 
-    def test_default_options_debug_log(self, select_effect_display_mock):
+    def test_default_options_debug_log(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--log-level', 'DEBUG'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn(" - DEBUG - Logging level enabled!", result.output)
         self.assertIn(" - INFO - Active Options(effect-display=CYCLE, effect-duration=10 secs, repeat-run=1, "
                       "brightness=8, invert=False, log-level=DEBUG, lead=False, effects_selected=ALL)", result.output)
-        select_effect_display_mock.assert_called_once()
+        effect_display_mock.select_effect_display.assert_called_once()
+        effect_display_mock.select_effect_display.return_value.render.assert_called_once()
 
-    def test_all_options_verbose_log(self, select_effect_display_mock):
+    def test_all_options_verbose_log(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['--effect-display', 'RANDOM',
                                                            '--effect-duration', '180',
                                                            '--repeat-run', '240',
@@ -86,16 +92,19 @@ class TestMain(TestCase):
         self.assertIn(" - INFO - Active Options(effect-display=RANDOM, effect-duration=180 secs, repeat-run=240, "
                       "brightness=3, invert=True, log-level=VERBOSE, lead=True, effects_selected=('Candle', 'Rainbow'))",
                       result.output)
-        select_effect_display_mock.assert_called_once()
+        effect_display_mock.select_effect_display.assert_called_once()
+        effect_display_mock.select_effect_display.return_value.render.assert_called_once()
 
-    def test_invalid_effect_name(self, select_effect_display_mock):
+    def test_invalid_effect_name(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['Rainbow', 'Unicorn'])
         self.assertEqual(result.exit_code, 2)
         self.assertIn('Error: Invalid value for "[EFFECTS_SELECTED]...": Unknown effect: Unicorn', result.output)
-        select_effect_display_mock.assert_not_called()
+        effect_display_mock.select_effect_display.assert_not_called()
+        effect_display_mock.select_effect_display.return_value.render.assert_not_called()
 
-    def test_invalid_effect_names(self, select_effect_display_mock):
+    def test_invalid_effect_names(self, effect_display_mock):
         result = self.runner.invoke(main.display_effects, ['Candle', 'Apple', 'Banana'])
         self.assertEqual(result.exit_code, 2)
         self.assertIn('Error: Invalid value for "[EFFECTS_SELECTED]...": Unknown effects: Apple, Banana', result.output)
-        select_effect_display_mock.assert_not_called()
+        effect_display_mock.select_effect_display.assert_not_called()
+        effect_display_mock.select_effect_display.return_value.render.assert_not_called()
