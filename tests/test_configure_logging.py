f5d9a01c7b9385c52a7173f74dad153fd926c706
John Chase <ratteal@gmail.com>
2019-08-29 18:10:58 +0100

    Correct logging testing.

--- tests/test_configure_logging.py
+++ tests/test_configure_logging.py
@@ -6,30 +6,44 @@ import ledshimdemo.configure_logging as cl
 
 class TestConfigureLogging(TestCase):
 
-    TEST_MESSAGE = "This is a test..."
-
-    @staticmethod
-    def get_log_name(log_level):
-        return cl.logging.getLevelName(log_level)
+    USER = "root"
+    DEBUG_TEST_MESSAGE = "This is a DEBUG test..."
+    VERBOSE_TEST_MESSAGE = "This is a VERBOSE test..."
+    INFO_TEST_MESSAGE = "This is a INFO test..."
+    WARNING_TEST_MESSAGE = "This is a WARNING test..."
 
     def test_invalid_log_level(self):
         with self.assertRaises(ValueError):
             cl.configure_logging("INVALID")
 
     def test_debug_log_level(self):
-        with LogCapture() as log_out:
-            cl.configure_logging(cl.logging.DEBUG)
-            cl.logging.debug(self.TEST_MESSAGE)
-        log_out.check(("root", self.get_log_name(cl.logging.DEBUG), self.TEST_MESSAGE),)
+        with LogCapture(level=cl.logging.DEBUG) as log_out:
+            cl.logging.debug(self.DEBUG_TEST_MESSAGE)
+            cl.verbose(self.VERBOSE_TEST_MESSAGE)
+        log_out.check((self.USER, cl.logging.getLevelName(cl.logging.DEBUG), self.DEBUG_TEST_MESSAGE),
+                      (self.USER, cl.logging.getLevelName(cl.VERBOSE), self.VERBOSE_TEST_MESSAGE),)
 
     def test_verbose_log_level(self):
-        with LogCapture() as log_out:
-            cl.configure_logging(cl.VERBOSE)
-            cl.verbose(self.TEST_MESSAGE)
-        log_out.check(("root", self.get_log_name(cl.VERBOSE), self.TEST_MESSAGE),)
+        with LogCapture(level=cl.VERBOSE) as log_out:
+            cl.logging.debug(self.DEBUG_TEST_MESSAGE)
+            cl.verbose(self.VERBOSE_TEST_MESSAGE)
+            cl.logging.info(self.INFO_TEST_MESSAGE)
+        log_out.check((self.USER, cl.logging.getLevelName(cl.VERBOSE), self.VERBOSE_TEST_MESSAGE),
+                      (self.USER, cl.logging.getLevelName(cl.logging.INFO), self.INFO_TEST_MESSAGE),)
 
     def test_info_log_level(self):
-        with LogCapture() as log_out:
-            cl.configure_logging(cl.logging.INFO)
-            cl.logging.info(self.TEST_MESSAGE)
-        log_out.check(("root", self.get_log_name(cl.logging.INFO), self.TEST_MESSAGE),)
+        with LogCapture(level=cl.logging.INFO) as log_out:
+            cl.logging.debug(self.DEBUG_TEST_MESSAGE)
+            cl.verbose(self.VERBOSE_TEST_MESSAGE)
+            cl.logging.info(self.INFO_TEST_MESSAGE)
+            cl.logging.warning(self.WARNING_TEST_MESSAGE)
+        log_out.check((self.USER, cl.logging.getLevelName(cl.logging.INFO), self.INFO_TEST_MESSAGE),
+                      (self.USER, cl.logging.getLevelName(cl.logging.WARNING), self.WARNING_TEST_MESSAGE),)
+
+    def test_warning_log_level(self):
+        with LogCapture(level=cl.logging.WARNING) as log_out:
+            cl.logging.debug(self.DEBUG_TEST_MESSAGE)
+            cl.verbose(self.VERBOSE_TEST_MESSAGE)
+            cl.logging.info(self.INFO_TEST_MESSAGE)
+            cl.logging.warning(self.WARNING_TEST_MESSAGE)
+        log_out.check((self.USER, cl.logging.getLevelName(cl.logging.WARNING), self.WARNING_TEST_MESSAGE),)
