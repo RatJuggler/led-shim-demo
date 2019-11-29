77906fec90fc00a7eb1210fd6e16c9a57e34f07e
John Chase <ratteal@gmail.com>
2019-11-29 15:50:33 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ledshimdemo',
-    version='2.0.1',
+    version='2.0.2',
     description='Show various effects on a Pimoroni LED shim.',
     long_description=long_description,
     long_description_content_type='text/markdown',
